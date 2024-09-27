c0f463c9a4a12e6105b409e9a307c7c22453c410
Tikhonov Alexander <tadphp@yandex.ru>
2024-09-27 19:34:02 +0300

    chore lab5

--- files/scripts.js
+++ files/scripts.js
@@ -132,17 +132,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
     renderItems();
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    const menuLinks = document.querySelectorAll('.menu-list a');
-
-    const currentUrl = window.location.pathname.split('/').pop();
-
-    menuLinks.forEach(link => {
-        const linkUrl = link.getAttribute('href').split('/').pop();
-
-        if (linkUrl === currentUrl) {
-            link.classList.add('active');
-        }
-    });
-});
